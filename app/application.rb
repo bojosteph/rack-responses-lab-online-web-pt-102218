552c821d1aacc51af23e06c478d2c2719bbf37e0
Roberto Samia <bojosteph@gmail.com>
2019-02-04 01:19:20 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if Time.now.hour.between?(0,11)
       resp.write "Good Morning"
     else
-      res.write "Good Afternoon"
+      resp.write "Good Afternoon"
     end
     
     resp.finish
